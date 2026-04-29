72dd2040679daa5a66abc139a712b25b1d79469b
shadi-123 <kgkzqnwcq7@privaterelay.appleid.com>
2026-04-29 14:52:07 +0300

    Update Medicine.java

--- Medicine.java
+++ Medicine.java
@@ -30,3 +30,69 @@ package main;
 	    }
 	}
 
+
+
+package main;
+import java.util.Scanner;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PharmacyManager manager = new PharmacyManager(100);
+        
+        // 1. نظام تسجيل الدخول (مطلب إلزامي)
+        System.out.println("=== Pharmacy System Login ===");
+        System.out.print("Username: ");
+        String user = sc.next();
+        System.out.print("Password: ");
+        String pass = sc.next();
+
+        if (!(user.equals("admin") && pass.equals("123"))) {
+            System.out.println("Wrong credentials. Exiting...");
+            return;
+        }
+
+        // 2. القائمة الرئيسية (Menu-based)
+        int choice;
+        do {
+            System.out.println("\n--- MAIN MENU ---");
+            System.out.println("1. Add General Medicine");
+            System.out.println("2. Add Prescription Medicine");
+            System.out.println("3. Search Medicine");
+            System.out.println("4. Update Price");
+            System.out.println("5. Save & Exit");
+            System.out.print("Select an option: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Name: "); String n = sc.next();
+                    System.out.print("Price: "); double p = sc.nextDouble();
+                    manager.addMedicine(new Medicine(n, p, MedicineCategory.GENERAL));
+                    break;
+                case 2:
+                    System.out.print("Name: "); String pn = sc.next();
+                    System.out.print("Price: "); double pp = sc.nextDouble();
+                    System.out.print("Doctor Name: "); String d = sc.next();
+                    manager.addMedicine(new PrescriptionMedicine(pn, pp, MedicineCategory.ANTIBIOTIC, d));
+                    break;
+                case 3:
+                    System.out.print("Enter name to search: ");
+                    manager.searchByName(sc.next());
+                    break;
+                case 4:
+                    System.out.print("Enter name: "); String un = sc.next();
+                    System.out.print("New Price: "); double up = sc.nextDouble();
+                    manager.updatePrice(un, up);
+                    break;
+                case 5:
+                    manager.saveData();
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+            }
+        } while (choice != 5);
+    }
+}
+
